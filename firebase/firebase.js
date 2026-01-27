0f5d3f6a105a98db07238675b01ddb2ee1375773
Kanyanta Pythias <kanyantapythias072@gmail.com>
2026-01-27 22:07:15 +0200

    Choose account on firebase login

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -22,5 +22,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 
 export { app, analytics, auth, googleProvider };
